5b70a99a75eeed5b2f5962ada5012be5b2794003
unknown <siegel.tucker@gmail.com>
2019-04-11 17:08:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ from traffic_light import TrafficLight, RED, YELLOW, GREEN
 
 tl = TrafficLight()
 
-tl.flash_random(15, 0.1)
+#tl.flash_random(15, 0.1)
+
+tl.start(RED)
+tl.flash((RED, YELLOW))
+
 
 tl.end()

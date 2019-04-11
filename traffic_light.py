4227ecae205689a8c46be07d6267d918d0514fc1
unknown <siegel.tucker@gmail.com>
2019-04-11 16:42:33 -0400

    Create traffic_light.py

---
+++ traffic_light.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO 
+import time 
+
+GPIO.setmode(GPIO.BCM)
+
+class TrafficLight():
+
+    def __init__(self):
+        pass
+
+    def start_green(self):
+        GPIO.setup(21, GPIO.OUT)
+
+    def kill_green(self):
+        GPIO.setup(21, GPIO.IN)
+
+    def flash_green(self, interval):
+        self.start_green()
+        time.sleep(interval)
+        self.kill_green()

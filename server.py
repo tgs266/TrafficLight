2a1422e2548873c881696a0ff4e1ef17e6bf4bc3
unknown <siegel.tucker@gmail.com>
2019-04-22 19:57:12 -0400

    update

--- server.py
+++ server.py
@@ -34,26 +34,44 @@ def test1():
 rec_data = []
 time_start = 0
 recent = 0
+done = True
 
 @app.route("/record", methods=['GET', 'POST'])
 def record():
     print(request.method)
     if request.method == 'POST':
-        global rec_data, time_start, recent
-        if request.form.get("record") == "Start" and time_start == 0:
+        global rec_data, time_start, recent, done
+        if request.form.get("record") == "Start" and time_start == 0 and done == False:
 
                 time_start = time.time()
                 recent = time_start
-        elif request.form.get("red") == "Red":
+        elif request.form.get("red") == "Red" and done == False:
             rec_data.append([RED, time.time() - recent])
             recent = time.time()
 
-        elif request.form.get("yellow") == "Yellow":
+        elif request.form.get("yellow") == "Yellow" and done == False:
             rec_data.append([YELLOW, time.time() - recent])
             recent = time.time()
-        elif request.form.get("green") == "Green":
+        elif request.form.get("green") == "Green" and done == False:
             rec_data.append([GREEN, time.time() - recent])
             recent = time.time()
+        
+        if time.time() - time_start > 30:
+            time_start = 0
+            recent = 0
+            done = True 
+
+        if request.form.get("record") == "Stop":
+            time_start = 0
+            recent = 0
+            done = True 
+        
+        if request.form.get("play") == "Play":
+            for i in rec_data:
+                tl.start(i[0])
+                time.sleep(i[1])
+            done = False 
+
         print(rec_data)
     return render_template("record.html")
  

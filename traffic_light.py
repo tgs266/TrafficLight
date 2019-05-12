25324da2cae6c986470074f8d58a2fb492a9096c
unknown <siegel.tucker@gmail.com>
2019-05-12 14:01:46 -0400

    Update traffic_light.py

--- traffic_light.py
+++ traffic_light.py
@@ -20,7 +20,7 @@ class TrafficLight():
         GPIO.cleanup()
 
     def start(self, channels):
-        GPIO.output(channels, GPIO.HIGH)
+        GPIO.setup(channels, GPIO.HIGH)
         if type(channels) == type([]) or type(channels) == type(()):
             for i in channels:
                 self.live[i] = 1
@@ -28,7 +28,7 @@ class TrafficLight():
             self.live[channels] = 1
 
     def kill(self, channels):
-        GPIO.output(channels, GPIO.LOW)
+        GPIO.setup(channels, GPIO.LOW)
         if type(channels) == type([]) or type(channels) == type(()):
             for i in channels:
                 self.live[i] = -1

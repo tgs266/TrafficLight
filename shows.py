f413ede87c41dcfb1138c895ae6d5a4e9930aac7
unknown <siegel.tucker@gmail.com>
2019-05-12 13:46:09 -0400

    add try

--- shows.py
+++ shows.py
@@ -1,22 +1,28 @@
 from traffic_light import TrafficLight, RED, YELLOW, GREEN
 import time
-tl = TrafficLight()
 
-tl.kill((RED, YELLOW, GREEN))
-tl.flash_random(5, 0.1)
-tl.kill((RED, YELLOW, GREEN))
-time.sleep(1)
-tl.cycle_down(5, 0.5)
-tl.kill((RED, YELLOW, GREEN))
-time.sleep(1)
-tl.cycle_up(5, 0.5)
-tl.kill((RED, YELLOW, GREEN))
-time.sleep(1)
-tl.build_up(5, 0.25)
-tl.kill((RED, YELLOW, GREEN))
-time.sleep(1)
-tl.build_down(5, 0.25)
-tl.kill((RED, YELLOW, GREEN))
-time.sleep(1)
-tl.slide_up(5, 0.25)
-tl.kill((RED, YELLOW, GREEN))
+tl = TrafficLight()
+try:
+
+    tl.kill((RED, YELLOW, GREEN))
+    tl.flash_random(5, 0.1)
+    tl.kill((RED, YELLOW, GREEN))
+    time.sleep(1)
+    tl.cycle_down(5, 0.5)
+    tl.kill((RED, YELLOW, GREEN))
+    time.sleep(1)
+    tl.cycle_up(5, 0.5)
+    tl.kill((RED, YELLOW, GREEN))
+    time.sleep(1)
+    tl.build_up(5, 0.25)
+    tl.kill((RED, YELLOW, GREEN))
+    time.sleep(1)
+    tl.build_down(5, 0.25)
+    tl.kill((RED, YELLOW, GREEN))
+    time.sleep(1)
+    tl.slide_up(5, 0.25)
+    tl.kill((RED, YELLOW, GREEN))
+except:
+    tl.end()
+
+

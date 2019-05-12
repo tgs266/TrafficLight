3ef46759ca7ceacc083f5a5f1394fb81d4cedf26
unknown <siegel.tucker@gmail.com>
2019-05-12 11:55:31 -0400

    change

--- light_shows.py
+++ light_shows.py
@@ -6,8 +6,4 @@ def show_1(length, interval):
     tl.flash_random(length, interval)
 
 def show_2():
-    tl.flash(RED, 1)
-    tl.start(RED)
-    time.sleep(.5)
-    tl.flash(YELLOW, 1)
-    tl.kill(RED)
+    tl.cycle_down(5, 0.5)

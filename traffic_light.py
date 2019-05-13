c1c3d796f3d6153f8959a9c4d3cfac2e2a357933
unknown <siegel.tucker@gmail.com>
2019-05-12 22:45:16 -0400

    test

--- traffic_light.py
+++ traffic_light.py
@@ -12,9 +12,9 @@ class TrafficLight():
 
     def __init__(self):
         self.live = {RED: -1, YELLOW: -1, GREEN: -1}  # -1 -> dead | 1 -> alive
-        GPIO.setup(RED, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(YELLOW, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(GREEN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(RED, GPIO.IN)
+        GPIO.setup(YELLOW, GPIO.IN)
+        GPIO.setup(GREEN, GPIO.IN)
 
     def end(self):
         GPIO.cleanup()
